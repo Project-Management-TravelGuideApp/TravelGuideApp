4cd2983f53d807c02eda8372b5c34bbc22027458
ZeynepUzmez <83713406+ZeynepUzmez@users.noreply.github.com>
2023-05-08 12:07:58 +0300

    firstpage front
    firstpage front butonları ikonlar ve navigatorların eklenmesi

--- src/pages/auth/FirstPage/FirstPage.js
+++ src/pages/auth/FirstPage/FirstPage.js
@@ -3,9 +3,38 @@ import { Text, View } from 'react-native';
 
 const FirstPage = () => {
     return (
-      <View>
-        <Text>FirstPage</Text>
+      <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('../../../components/assets/logo_transparent.png')}
+      />
+      <Text style={styles.header}>Travel Guide App</Text>
+      <View style={styles.body_container}>
+        <TouchableOpacity
+          style={styles.google}
+        >
+          <Icon name="google" size={35} />
+          <Text style={styles.google_text}>Google İle Oturum Aç</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.facebook}
+          >
+          <Icon name="facebook" size={40} />
+          <Text style={styles.facebook_text}>Facebook İle Oturum Aç</Text>
+        </TouchableOpacity>
+        <Button onPress={() => navigation.navigate('SignIn')} text="Kayıt Ol" />
       </View>
+      <View style={styles.footer_container}>
+        <Text style={styles.question_text}>Zaten bir hesabınız var mı?</Text>
+        <TouchableOpacity style={styles.login}>
+          <Text
+            style={styles.login_text}
+            onPress={() => navigation.navigate('Login')}>
+            Giriş Yap
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
     );
 }
 
